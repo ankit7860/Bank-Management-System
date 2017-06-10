dbf1c17be5ed5a23e452abab16b5a94126130747
ankit7860 <ankit.pal2011@gmail.com>
2017-06-10 15:48:32 +0530

    Update PerformTransactionDAOImpl.java

--- BaseCodeSlice_BankManagementSystem/src/com/bankmanagement/dao/PerformTransactionDAOImpl.java
+++ BaseCodeSlice_BankManagementSystem/src/com/bankmanagement/dao/PerformTransactionDAOImpl.java
@@ -1,12 +1,12 @@
-package com.bankmanagement.dao;
+package com.cts.bankmanagement.dao;
 
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.bankmanagement.entity.TransactionUser;
-import com.bankmanagement.vo.TransactionVO;
+import com.cts.bankmanagement.entity.TransactionUser;
+import com.cts.bankmanagement.vo.TransactionVO;
 
 @Repository("performTransactionDAO")
 public class PerformTransactionDAOImpl implements PerformTransactionDAO {
@@ -24,11 +24,15 @@ public class PerformTransactionDAOImpl implements PerformTransactionDAO {
 	public Long updateTransactionDetails(TransactionVO transactionVO) {
 		TransactionUser transaction = addPerformTransaction(transactionVO);
 		Long transactionId = (Long) sessionFactory.getCurrentSession().save(transaction);
+		sessionFactory.getCurrentSession()
+				.createQuery("update TransactionUser set transactionId = :transactionId where transactionId = :transId")
+				.setParameter("transactionId", transactionVO.getTransactionId()).setParameter("transId", transactionId)
+				.executeUpdate();
 		return transactionId;
 	}
 
 	@Override
-	public Long getTransactionId() {
+	public Long getTransactionCount() {
 		Query query = sessionFactory.getCurrentSession()
 				.createQuery("Select count(TRANSACTION_ID) from TransactionUser");
 		Long count = (Long) query.uniqueResult();
